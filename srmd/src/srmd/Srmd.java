557f24483358a694e6002b8b4934b0c5595fc914
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-17 22:03:45 -0400

    Add files via upload

---
+++ srmd/src/srmd/Srmd.java
@@ -0,0 +1,21 @@
+package srmd;
+public class Srmd {
+    public static void main(String[] args) {
+        int n1,n2,suma,resta,mult;
+        double n3,n4;
+        double div;
+        n1 = 5;
+        n2 = 6; 
+        n3 = 5;
+        n4 = 6; 
+        
+        suma = n1+n2;
+        resta = n1-n2;
+        mult = n1*n2;
+        div = n3/n4;
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(mult);
+        System.out.println(div);
+    }   
+}
