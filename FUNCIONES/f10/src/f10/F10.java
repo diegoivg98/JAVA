b9b7c8334df143e4ed5baac5c7eba6e4746299eb
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-20 22:26:28 -0400

    Add files via upload

---
+++ FUNCIONES/f10/src/f10/F10.java
@@ -0,0 +1,10 @@
+package f10;
+public class F10 {
+    public static void main(String[] args) {
+        System.out.println(texto("diego"));
+        System.out.println(texto("ANImal"));
+    }
+    public static String texto(String t){
+        return t.toUpperCase();
+    }
+}
---
+++ FUNCIONES/f9/src/f9/F9.java
@@ -0,0 +1,14 @@
+package f9;
+public class F9 {
+    public static void main(String[] args) {
+        int r1 = largo("diego");
+        int r2 = largo("neumatico");
+        System.out.println(r1);
+        System.out.println(r2);
+    }
+    public static int largo (String t){
+       int res;
+        res = t.length();
+        return res;
+    }
+}
