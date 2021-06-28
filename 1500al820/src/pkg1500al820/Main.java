97ad9cd8cc9593320cd19fd54857ec605fc6abff
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-27 22:47:32 -0400

    Delete 1500al820 directory

--- 1500al820/src/pkg1500al820/Main.java
+++
@@ -1,8 +0,0 @@
-package pkg1500al820;
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 1500; i >= 820; i--) {
-            System.out.println(i);
-        }
-    }
-}
