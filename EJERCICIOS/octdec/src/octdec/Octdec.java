98259cc9622fa179c890e89482d9b7802148c35d
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-28 02:45:22 -0400

    Add files via upload

---
+++ EJERCICIOS/octdec/src/octdec/Octdec.java
@@ -0,0 +1,21 @@
+package octdec;
+
+import javax.swing.JOptionPane;
+
+public class Octdec {
+    public static void main(String[] args) {
+        int num = 0;
+        int decimal = 0;
+        int exp = 0;
+        num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
+        System.out.println("NUMERO OCTAL: "+num);
+        
+        while (num!=0){
+            int res = num % 10;
+            decimal += res*Math.pow(8, exp);
+            exp++;
+            num /= 10;         
+        }
+        System.out.println("NUMERO DECIMAL: "+decimal);
+    }   
+}
