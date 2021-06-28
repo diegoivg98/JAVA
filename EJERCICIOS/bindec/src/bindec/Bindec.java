6cca6bc0a98029a9f226ed68aa9f1c4e0d93496a
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-28 00:00:03 -0400

    Add files via upload

---
+++ EJERCICIOS/bindec/src/bindec/Bindec.java
@@ -0,0 +1,17 @@
+package bindec;
+import javax.swing.JOptionPane;
+public class Bindec {
+    public static void main(String[] args) {
+        int num,ult_dig,cont=0,decimal=0;
+        num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
+        System.out.println("NUMERO BINARIO: "+num);
+        
+        while(num!=0){
+            ult_dig = num%10;
+            decimal += (ult_dig * Math.pow(2, cont));
+            cont++;
+            num /= 10;
+        }
+        System.out.println("NUMERO DECIMAL: "+decimal);
+    }
+}   
