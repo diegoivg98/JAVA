9fab5d93199a66c41dae457e41a4cc5dc244ab06
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-28 02:09:28 -0400

    Update Decbin.java

--- EJERCICIOS/decbin/src/decbin/Decbin.java
+++ EJERCICIOS/decbin/src/decbin/Decbin.java
@@ -6,7 +6,6 @@ public class Decbin {
         int exp = 0;
         int num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         System.out.println("NUMERO DECIMAL: "+num);
-        ArrayList<Integer> lista = new ArrayList<Integer>();
         while(num!=0){
             int res = num % 2;
             bin += res*Math.pow(10, exp);
