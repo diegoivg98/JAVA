6e8abc7ee17586eeeef921f4b4d70c0d6cf1b37a
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-02 17:37:36 -0400

    Delete EJERCICIOS/749al1221 directory

--- EJERCICIOS/749al1221/src/pkg749al1221/Main.java
+++
@@ -1,8 +0,0 @@
-package pkg749al1221;
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 749; i <= 1221; i++) {
-            System.out.println(i);
-        }
-    }   
-}
