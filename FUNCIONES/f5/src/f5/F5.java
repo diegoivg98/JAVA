872e3a6550b58f035cd52a77d663b5ef5ef84d47
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:01:42 -0400

    Update F5.java

--- FUNCIONES/f5/src/f5/F5.java
+++ FUNCIONES/f5/src/f5/F5.java
@@ -1,12 +1,11 @@
 package f5;
-
 import javax.swing.JOptionPane;
-
 public class F5 {
     public static void main(String[] args) {
        String a = JOptionPane.showInputDialog("INGRESA SI, NO O OTRA PALABRA");
         System.out.println(respuesta(a));
     }
+    //FUNCION QUE VALIDA SI LA PALABRA INGRESA ES UN SI O NO, SI CUMPLE RETORNA "CORRECTO", NO CUMPLIR RETORNA "INCORRECTO"
     public static String respuesta(String r){
         if ("si".equals(r) || "no".equals(r)){
             r = "CORRECTO";
