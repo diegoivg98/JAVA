edd20e7cdab255344dab21565281fc65c7f4dd70
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:07:32 -0400

    Update Main.java

--- bucle/749al1221/src/pkg749al1221/Main.java
+++ bucle/749al1221/src/pkg749al1221/Main.java
@@ -2,7 +2,7 @@ package pkg749al1221;
 public class Main {
     public static void main(String[] args) {
         for (int i = 749; i <= 1221; i++) {
-            System.out.println(i);
+            System.out.println(i);//IMPRIMIR NUMEROS DEL 749 AL 1221
         }
     }   
 }
