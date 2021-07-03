57610cdca8d06cc8067adb25927c4cdfa436a444
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:04:41 -0400

    Update F8.java

--- FUNCIONES/f8/src/f8/F8.java
+++ FUNCIONES/f8/src/f8/F8.java
@@ -2,13 +2,15 @@ package f8;
 import javax.swing.JOptionPane;
 public class F8 {
     public static void main(String[] args) {
-        double t = Double.parseDouble(JOptionPane.showInputDialog("INGRESA UNA CANTIDAD DE DOLARES"));
+        double t = Double.parseDouble(JOptionPane.showInputDialog("INGRESA UNA CANTIDAD DE GRADOS: "));
         double t1=temperatura((int) t,1);
         double t2=temperatura((int) t,2);
         System.out.println(t1+" °FARENHEIT");
         System.out.println(t2+" °KELVIN");
-        
     }
+    //FUNCION QUE REALICE LA CONVERSION DE CELSIUS A FARENHEIT O KELVIN
+    //X ES LA CANTIDAD DE GRADOS
+    //Y OPCION DE CONVERSION
     public static double temperatura(int x , int y){
         int res = 0;
         if (y==1) {
