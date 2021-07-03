6a6718dc1142a2a45f077cba08f5efd4212f49d6
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:13:52 -0400

    Update Main.java

--- condiciones/3notas/src/pkg3notas/Main.java
+++ condiciones/3notas/src/pkg3notas/Main.java
@@ -1,9 +1,8 @@
 package pkg3notas;
-
 import javax.swing.JOptionPane;
-
 public class Main {
     public static void main(String[] args) {
+        //INGRESAR 3 NOTAS, VERIFICAR SI APRUEBA O REPRUEBA
         double n1,n2,n3,prom;
         n1 = Double.parseDouble(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         n2 = Double.parseDouble(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
