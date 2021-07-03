fe69cd39c2a4cb7da2c19d5f353f093e1d43e4f2
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 01:46:09 -0400

    Update Bindec.java

--- EJERCICIOS/bindec/src/bindec/Bindec.java
+++ EJERCICIOS/bindec/src/bindec/Bindec.java
@@ -5,7 +5,7 @@ public class Bindec {
         int num,ult_dig,cont=0,decimal=0;
         num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         System.out.println("NUMERO BINARIO: "+num);
-        
+        //CONVERTIR NUMERO BINARIO A DECIMAL
         while(num!=0){
             ult_dig = num%10;
             decimal += (ult_dig * Math.pow(2, cont));
