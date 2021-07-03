c5deb307311caaee9b3fe67ab019745427f469c5
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:16:44 -0400

    Update Parimpar.java

--- condiciones/parimpar/src/parimpar/Parimpar.java
+++ condiciones/parimpar/src/parimpar/Parimpar.java
@@ -1,9 +1,8 @@
 package parimpar;
-
 import javax.swing.JOptionPane;
-
 public class Parimpar {
     public static void main(String[] args) {
+        //INGRESAR UN NUMERO, VERIFICAR SI ES PAR O IMPAR
         int num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO: "));
         if (num % 2 == 0) {
             System.out.println("PAR");
