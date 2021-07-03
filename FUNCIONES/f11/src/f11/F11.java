3fda7002293d63fc05690ba9a97638a00fb0625e
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 01:53:23 -0400

    Update F11.java

--- FUNCIONES/f11/src/f11/F11.java
+++ FUNCIONES/f11/src/f11/F11.java
@@ -5,6 +5,8 @@ public class F11 {
         System.out.println(cambioclp(1,2));
         System.out.println(cambioclp(1,3));
     }
+    //CREAR FUNCION QUE DEVUELVA LA CONVERSION DE PESOS CHILENOS A DOLAR, EURO O YENES
+    //Y ES LA OPCION A CONVERTIR, X ES LA CANTIDAD DE PESOS CHILENOS
     public static double cambioclp (double x, double y){
         if (y==1) {
             return x*0.0014;//DOLAR
