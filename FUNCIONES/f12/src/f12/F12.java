e72e39ca0fb38e715f98e4ba801f8b81240c0894
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 01:54:34 -0400

    Update F12.java

--- FUNCIONES/f12/src/f12/F12.java
+++ FUNCIONES/f12/src/f12/F12.java
@@ -3,6 +3,7 @@ public class F12 {
     public static void main(String[] args) {
         cuadrado(3);
     }
+    //FUNCION QUE RETORNA EL AREA Y PERIMETRO DE UN CUADRADO
     public static int cuadrado (int x){
         int area = x*x;
         int perimetro = x*4;
