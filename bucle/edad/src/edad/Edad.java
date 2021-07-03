522cd2629358664a3b2c12945a60f51f67429dad
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:08:35 -0400

    Update Edad.java

--- bucle/edad/src/edad/Edad.java
+++ bucle/edad/src/edad/Edad.java
@@ -4,7 +4,7 @@ public class Edad {
     public static void main(String[] args) {
         int edad = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         for (int i = 0; i < edad; i++) {
-            System.out.println(i+1 +" "+"años");
+            System.out.println(i+1 +" "+"años");//IMPRIMIR CUANTOS AÑOS TIENES, CONTANDO DESDE EL 1
         }
     }   
 }
