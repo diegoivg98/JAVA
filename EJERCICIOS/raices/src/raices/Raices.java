512d986e4a0d8a424ecc9f516b06adbaa316db73
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 01:48:21 -0400

    Update Raices.java

--- EJERCICIOS/raices/src/raices/Raices.java
+++ EJERCICIOS/raices/src/raices/Raices.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 public class Raices {
     public static void main(String[] args) {
         double num = Double.parseDouble(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
+        //OBTENER LA RAIZ CUADRADA DE UN NUMERO
         System.out.println("la raiz de " +num+" es: "+Math.sqrt(num));
     }   
 }
