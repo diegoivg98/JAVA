bc4e76b3f71ade9ee0d7a1f60b7355fcbf218ead
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:16:13 -0400

    Update Edad.java

--- condiciones/edad/src/edad/Edad.java
+++ condiciones/edad/src/edad/Edad.java
@@ -1,9 +1,8 @@
 package edad;
-
 import javax.swing.JOptionPane;
-
 public class Edad {
     public static void main(String[] args) {
+        //INGRESAR UNA EDAD, SI ES >18 IMPRIMIR QUE ES MAYOR DE EDAD, DE LO COTRARIO IMPRIMIR QUE ES MENOR DE EDAD
         int edad = Integer.parseInt(JOptionPane.showInputDialog("INGRESE SU EDAD: "));
         if (edad < 18) {
             System.out.println("ERES MENOR DE EDAD");
