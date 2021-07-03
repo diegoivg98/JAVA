f4c2235732a690581474fc2c8c7357689441ebd5
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 01:56:07 -0400

    Update F13.java

--- FUNCIONES/f13/src/f13/F13.java
+++ FUNCIONES/f13/src/f13/F13.java
@@ -1,13 +1,12 @@
 package f13;
-
 import javax.swing.JOptionPane;
-
 public class F13 {
     public static void main(String[] args) {
         int a = Integer.parseInt(JOptionPane.showInputDialog("INGRESA UN NUMERO"));
         int b = Integer.parseInt(JOptionPane.showInputDialog("INGRESA UN NUMERO"));
         System.out.println(comp(a,b));
     }
+    //FUNCION QUE RETORNA 0 SI AMBOS NUMEROS SON IGUALES, 1 SI EL PRIMERO ES MAYOR QUE EL SEGUNDO, 2 LO CONTRARIO AL ANTERIOR
     public static int comp(int x, int y){
         if (x==y) {
             return 0;
