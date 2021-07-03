b4bae20d99fe2684100c219634ed55777a2494f5
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:06:53 -0400

    Update Main.java

--- bucle/1500al820/src/pkg1500al820/Main.java
+++ bucle/1500al820/src/pkg1500al820/Main.java
@@ -2,7 +2,7 @@ package pkg1500al820;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1500; i >= 820; i--) {
-            System.out.println(i);
+            System.out.println(i);//IMPRIMIR NUMEROS DEL 1500 AL 820
         }
     }
 }
