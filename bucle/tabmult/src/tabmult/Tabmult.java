738bbd961cfc14bb4318e09f0ccb60e16fe8d408
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:13:04 -0400

    Update Tabmult.java

--- bucle/tabmult/src/tabmult/Tabmult.java
+++ bucle/tabmult/src/tabmult/Tabmult.java
@@ -1,9 +1,8 @@
 package tabmult;
-
 import javax.swing.JOptionPane;
-
 public class Tabmult {
     public static void main(String[] args) {
+        //INGRESAR UN NUMERO, OBTENER SU TABLA DE MULTIPLICAR DEL 1 AL 10
        int num,res;
        num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         for (int i = 1; i < 11; i++) {
