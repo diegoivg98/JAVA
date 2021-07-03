db7f0d9c0163d35ce4c302574fc8c3a35611ab2c
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:00:29 -0400

    Update F4.java

--- FUNCIONES/f4/src/f4/F4.java
+++ FUNCIONES/f4/src/f4/F4.java
@@ -6,6 +6,7 @@ public class F4 {
         System.out.println(h1);
         System.out.println(h2);
     }
+    //FUNCION QUE VALIDA LA HORA, SI CUMPLE RETORNA "OK", DE NO CUMPLIR RETORNA "NO"
     public static String valhora (int h, int m, int s){
         if (h<=23 && m<=59 && s<=59 && h>=0 && m>=0 && s>=0){
             String r1 = "OK";
