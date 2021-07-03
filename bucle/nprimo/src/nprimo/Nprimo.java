61974fc817cff107b3fe5a17bd7d5f731a54eb9e
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:12:05 -0400

    Update Nprimo.java

--- bucle/nprimo/src/nprimo/Nprimo.java
+++ bucle/nprimo/src/nprimo/Nprimo.java
@@ -2,6 +2,7 @@ package nprimo;
 import javax.swing.JOptionPane;
 public class Nprimo {
     public static void main(String[] args) {
+        //INGRESAR UN NUMERO, VERIFICAR SI ES PRIMO O NO
         int num = Integer.parseInt(JOptionPane.showInputDialog("INGRESE UN NUMERO"));
         int i = 2;
         while(num % i != 0){
