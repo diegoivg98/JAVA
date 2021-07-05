d753347e6f86658875234647fb17caa8fa943dd3
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-05 01:37:13 -0400

    Update Formulageneral.java

--- EJERCICIOS/formulageneral/src/formulageneral/Formulageneral.java
+++ EJERCICIOS/formulageneral/src/formulageneral/Formulageneral.java
@@ -4,7 +4,7 @@ public class Formulageneral {
         int a=4,b=5,c=-3;
         double d = Math.pow(b, 2)- 4*a*c;//contenido dentro de la raiz
         double x1 = (-b+Math.sqrt(d))/(2*a);//solucion 1
-        double x2 = (b+Math.sqrt(d))/(2*a);//solucion 2
+        double x2 = (-b-Math.sqrt(d))/(2*a);//solucion 2
         System.out.println(x1);
         System.out.println(x2);
     }   
